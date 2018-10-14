9c8fca686d9b0061d0848c94f9b6e2ee4061e5f5
Head <mrdoo309@gmail.com>
2018-10-14 16:09:58 +0900

    해결 콜라츠, x만큼의 공백이 있는 다음수

--- fibo.js
+++ fibo.js
@@ -3,14 +3,14 @@ function solution(n) {
     function fibo (n) {
         if (n === 1 || n === 2) {
             obj[n] = 1
-            return obj[n]
+            return obj[n] % 12345677
         } else if(!obj[n]) {
             obj[n] = obj[n-2] + obj[n-1] || fibo(n - 2) + fibo(n - 1)
         }
-        return obj[n]
+        return obj[n] % 1234567
     }
     fibo(n)
-    return obj[n]
+    return obj[n] % 1234567
 }
 
 console.log(solution(80));
