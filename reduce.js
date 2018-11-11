426832925e93bd62e6756d74dbe6b1cf7406be5a
Head <mrdoo309@gmail.com>
2018-11-11 16:21:12 +0900

    change reduce func

--- reduce.js
+++ reduce.js
@@ -1,12 +1,20 @@
-const arr = [1,2,3,4,5]
+const arr = [
+    {obj:1},
+    {obj:2},
+    {obj:3},
+    {obj:4}
+]
 
 //what we make
-console.log(arr.reduce((a, b) => a + b, 10))
+console.log(arr.reduce((a, b) => {
+    a + b['obj']
+    return a + b['obj']
+}, 10))
 
 function reduce(arr, start) {
     let ret = start
     for(let i = 0; i < arr.length; i++) {
-        ret = ret + arr[i]
+        ret = ret + arr[i]['obj']
     }
     return ret
 }
