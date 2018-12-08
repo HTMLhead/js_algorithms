b49dbc253827e50f988edddb2b9a8ff9c37e4648
Head <mrdoo309@gmail.com>
2018-12-08 23:00:27 +0900

    solve mark and toys

--- hackerrank/MarkAndToys.js
+++ hackerrank/MarkAndToys.js
@@ -6,7 +6,7 @@ function maximumToys(prices, k) {
         toys++
         if(k < 0) break;
     }
-    return toys
+    return toys -1
 }
 
 console.log(maximumToys([1 ,12, 5, 111, 200, 1000, 10], 50))
