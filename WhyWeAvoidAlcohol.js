4eb8c7d4081da2ed223900c0e048429dd4a6cf61
Head <mrdoo309@gmail.com>
2018-12-10 11:51:43 +0900

    slove alcoholic

--- WhyWeAvoidAlcohol.js
+++ WhyWeAvoidAlcohol.js
@@ -2,7 +2,6 @@ var solution = function(day, drinkingDayArr) {
     var dayArr = new Array(day).fill(0)
     dayArr[0] = 1
     dayArr.forEach((v,i) => {
-        debugger
         let oneDay = drinkingDayArr.shift()
         if((i) === oneDay) {
             dayArr[i-1] = 0
@@ -16,4 +15,6 @@ var solution = function(day, drinkingDayArr) {
     return dayArr.reduce((a,b) => a + b)
 }
 
-export default solution
+console.log(solution(1111111, [11,222,444,555,666]))
+
+
