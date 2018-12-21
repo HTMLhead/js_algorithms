aa2264feda6d4f8aab4892ddc3d02c6a1afd1689
Head <mrdoo309@gmail.com>
2018-12-21 17:36:00 +0900

    merge

--- programmers/bestElbum.js
+++ programmers/bestElbum.js
@@ -1,25 +1,52 @@
 function solution(genres, plays) {
-    var obj = {}
+    // 1. 장르별 플레이 수 합 구하기
+    let genreSumObj = {};
     genres.forEach((genre, i) => {
-        if(obj[genre] === undefined) {
-            obj[genre] = plays[i]
-        } else {
-            obj[genre] = obj[genre] + plays[i]
+        !genreSumObj[genre] ? genreSumObj[genre] = plays[i] : genreSumObj[genre] += plays[i];
+    }
+    );
+
+    // 2. 합 수 정렬한 후, 순서별 장르 뽑기 
+    const sumOrder = Object.values(genreSumObj).sort((a, b) => b - a);
+    let genreOrder = [];
+    sumOrder.forEach(genre => {
+        for (let genre2 in genreSumObj) {
+            if (genre === genreSumObj[genre2]) genreOrder.push(genre2);
         }
-    })
+    });
     
-    var obj2 = {}
+    let obj = {}
+    
     genres.forEach((genre, i) => {
-        if(obj2[genre] === undefined) {
-            obj2[genre] = [plays[i]]
+        if(!obj[genre]) {
+            obj[genre] = [[plays[i], i]]
         } else {
-            obj2[genre].push(plays[i])
+            obj[genre].push([plays[i], i])
         }
     })
 
-    Object.keys(obj2).forEach(v => {
-        obj2[v].sort((a,b) => b-a) 
-    })  
+    for(let genre in obj) {
+        obj[genre].sort((a,b) => b[0]-a[0])
+    }
+
+    for(let genre in obj) {
+        if(obj[genre].length > 2) {
+            obj[genre].length = 2
+        }
+    }
+    
+    var ret = [];
+    genreOrder.forEach(genre => {
+        for(let j in obj) {
+            if(j === genre) {
+                ret.push(obj[j][0][1], obj[j][1][1])
+            }
+        }
+    })
+    return ret
 }
 
-solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])
+const genres = ["classic", "pop", "classic", "classic", "pop"]
+const plays = [500, 600, 150, 800, 2500]
+
+console.log(solution(genres, plays))
